e4d6d252c2240201f6a4712c7810c42681de927f
James Doyle <james2doyle@gmail.com>
2020-03-14 23:38:24 -0700

    add build output

---
+++ dist/loader.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const validateOptions = require('schema-utils');
+
+const {
+  getOptions
+} = require('loader-utils');
+
+const schema = {
+  type: 'object',
+  properties: {// name: {
+    //   type: 'string'
+    // }
+  }
+};
+
+module.exports = function loader(source) {
+  const callback = this.async();
+  const options = getOptions(this);
+  validateOptions(schema, options, 'Example Loader');
+  const output = source.replace(/\[name\]/g, options.name);
+  const results = `module.exports = ${JSON.stringify(output)}`;
+  callback(null, results);
+};
